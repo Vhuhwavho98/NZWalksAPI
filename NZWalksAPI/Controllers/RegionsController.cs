42954c3b71587ddbd05aac7d5d34971e998d3954
vhuhwavho Nevhuloro <vhurhwavho1@gmail.com>
2025-01-14 21:21:28 +0200

    Add project files.

---
+++ NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _nZWalksDbContext;
+
+        public RegionsController( NZWalksDbContext nZWalksDbContext)
+        {
+            _nZWalksDbContext = nZWalksDbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+
+            var regions = _nZWalksDbContext.regions.ToList();
+            return Ok(regions);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetRegionById(Guid id)
+        {
+            var region = _nZWalksDbContext.regions.Find(id);
+
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+        }
+    }
+}
---
+++ NZWalksAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> regions { get; set; }
+        public DbSet<Walk> walks { get; set; }  
+
+        public DbSet<Difficulty> difficulty { get; set; }
+    }
+}
